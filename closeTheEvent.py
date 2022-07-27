9a289131ec11b8d7e357b0c6acb575ec2c37e8be
adc881029 <adc881029@gmail.com>
2022-07-27 22:07:13 +0800

    準備推上heroku

--- closeTheEvent.py
+++ closeTheEvent.py
@@ -1,23 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 import pygsheets
 from datetime import datetime, timedelta
 import json
 import time
 now = datetime.now().strftime("%H")
-while(now!="44"):
+while(now!="8"):
     print(now)
     time.sleep(60)
     now = datetime.now().strftime("%H")
 print('end')
-
-
-# In[ ]:
-
-
-
-
+gc = pygsheets.authorize(service_file='./creds.json')
+survey_url = 'https://docs.google.com/spreadsheets/d/1QwnPezCF8T_TJgY9C0vTa_lykLgXF2Y-WEYPISwRZkA/'
+sh = gc.open_by_url(survey_url)
+testData=sh.worksheet('title','testData')
+testData.update_col(0, values=[now], row_offset=0)
