252ed835cd24927a8e2e34c61b13d0ed9e48009a
adc881029 <adc881029@gmail.com>
2022-07-29 22:39:40 +0800

    可以正確更新數據

--- closeTheEvent.py
+++ closeTheEvent.py
@@ -2,14 +2,18 @@ import pygsheets
 from datetime import datetime, timedelta
 import json
 import time
-now = datetime.now().strftime("%H")
+now = datetime.utcnow().strftime("%H")
+''''''
 while(now!="00" and now!="24"):
     #print(now)
-    time.sleep(60)
-    now = datetime.now().strftime("%H")
+    time.sleep(600)
+    now = datetime.utcnow().strftime("%H")
 print('end')
+
 gc = pygsheets.authorize(service_file='./creds.json')
 survey_url = 'https://docs.google.com/spreadsheets/d/1QwnPezCF8T_TJgY9C0vTa_lykLgXF2Y-WEYPISwRZkA/'
 sh = gc.open_by_url(survey_url)
-testData=sh.worksheet('title','testData')
-testData.update_col(0, values=[now], row_offset=0)
+newData=sh.worksheet('title','WeightTest')
+oldData=sh.worksheet('title','CakemonDataTest')
+newWeight=newData.get_col(2,include_tailing_empty=False)
+oldData.update_col(2, values=newWeight, row_offset=0)
