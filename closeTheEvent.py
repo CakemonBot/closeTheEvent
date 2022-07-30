c0be217a5c40da1b5131dd9414e3eadebb311410
adc881029 <adc881029@gmail.com>
2022-07-30 10:16:47 +0800

    測試網址

--- closeTheEvent.py
+++ closeTheEvent.py
@@ -11,7 +11,7 @@ while(now!="00" and now!="24"):
 print('end')
 
 gc = pygsheets.authorize(service_file='./creds.json')
-survey_url = 'https://docs.google.com/spreadsheets/d/1QwnPezCF8T_TJgY9C0vTa_lykLgXF2Y-WEYPISwRZkA/'
+survey_url = 'https://docs.google.com/spreadsheets/d/1QwnPezCF8T_TJgY9C0vTa_lykLgXF2Y-WEYPISwRZkA/edit?fbclid=IwAR2oDcXlmmfvC90cSgog-rU7nYjDrpBY6chkcmjxoyan-lS0zdpPSWJBw1k#gid=1852814271'
 sh = gc.open_by_url(survey_url)
 newData=sh.worksheet('title','WeightTest')
 oldData=sh.worksheet('title','CakemonDataTest')
