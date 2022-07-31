82d428c34f6b01570565bc928cbb35129e4bcdba
adc881029 <adc881029@gmail.com>
2022-07-31 18:58:31 +0800

    正式版本

--- closeTheEvent.py
+++ closeTheEvent.py
@@ -14,7 +14,7 @@ gc = pygsheets.authorize(service_file='./creds.json')
 survey_url = 'https://docs.google.com/spreadsheets/d/1QwnPezCF8T_TJgY9C0vTa_lykLgXF2Y-WEYPISwRZkA/edit?fbclid=IwAR2oDcXlmmfvC90cSgog-rU7nYjDrpBY6chkcmjxoyan-lS0zdpPSWJBw1k#gid=1852814271'
 sh = gc.open_by_url(survey_url)
 newData=sh.worksheet('title','WeightTest')
-oldData=sh.worksheet('title','CakemonDataTest')
+oldData=sh.worksheet('title','CakemonData')
 newWeight=newData.get_col(2,include_tailing_empty=False)
 oldData.update_col(2, values=newWeight, row_offset=0)
 while(True):
